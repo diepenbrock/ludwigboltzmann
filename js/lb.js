c21e5d363d21531dfa9188c0d93f2322cec7c47c
Ted Diepenbrock <unoirok08@gmail.com>
2017-12-22 15:56:27 -0600

    anopro basic design

--- js/lb.js
+++ js/lb.js
@@ -3,3 +3,112 @@
 
 
 
+function testing() {
+    var a = document.getElementById('a');
+    a.addEventListener('click', function() {
+        a.innerHTML = '46 years old'
+    });
+    var b = document.getElementById('b');
+    b.addEventListener('click', function() {
+        b.innerHTML = '682,545 people'
+    });
+    var c = document.getElementById('c');
+    c.addEventListener('click', function() {
+        c.innerHTML = '80.7%'
+    });
+    var d = document.getElementById('d');
+    d.addEventListener('click', function() {
+        d.innerHTML = '29,029ft'
+    });
+    var e = document.getElementById('e');
+    e.addEventListener('click', function() {
+        e.innerHTML = '5.4ft'
+    });
+    var f = document.getElementById('f');
+    f.addEventListener('click', function() {
+        f.innerHTML = '1,250ft or 1,454ft to tip'
+    });
+    var g = document.getElementById('g');
+    g.addEventListener('click', function() {
+        g.innerHTML = '1.324billion'
+    });
+    var h = document.getElementById('h');
+    h.addEventListener('click', function() {
+        h.innerHTML = '36,070ft'
+    });
+    var i = document.getElementById('i');
+    i.addEventListener('click', function() {
+        i.innerHTML = '-128.6 F'
+    });
+    var j = document.getElementById('j');
+    j.addEventListener('click', function() {
+        j.innerHTML = '171,476 words'
+    });
+    var k = document.getElementById('k');
+    k.addEventListener('click', function() {
+        k.innerHTML = '195'
+    });
+    var l = document.getElementById('l');
+    l.addEventListener('click', function() {
+        l.innerHTML = '89.8 billion USD'
+    });
+    var m = document.getElementById('m');
+    m.addEventListener('click', function() {
+        m.innerHTML = '2hr 10min'
+    });
+    var n = document.getElementById('n');
+    n.addEventListener('click', function() {
+        n.innerHTML = '3.0GPA'
+    });
+    var o = document.getElementById('o');
+    o.addEventListener('click', function() {
+        o.innerHTML = '7'
+    });
+    var p = document.getElementById('p');
+    p.addEventListener('click', function() {
+        p.innerHTML = '57,466 USD'
+    });
+    var q = document.getElementById('q');
+    q.addEventListener('click', function() {
+        q.innerHTML = '78 years'
+    });
+    var r = document.getElementById('r');
+    r.addEventListener('click', function() {
+        r.innerHTML = '40 minutes'
+    });
+    var s = document.getElementById('s');
+    s.addEventListener('click', function() {
+        s.innerHTML = 'six months'
+    });
+    var t = document.getElementById('t');
+    t.addEventListener('click', function() {
+        t.innerHTML = '$51,939'
+    });
+    var u = document.getElementById('u');
+    u.addEventListener('click', function() {
+        u.innerHTML = '28 days'
+    });
+    var v = document.getElementById('v');
+    v.addEventListener('click', function() {
+        v.innerHTML = '12ft 11in'
+    });
+    var w = document.getElementById('w');
+    w.addEventListener('click', function() {
+        w.innerHTML = '40-50%'
+    });
+    var x = document.getElementById('x');
+    x.addEventListener('click', function() {
+        x.innerHTML = '60mph'
+    });
+    var y = document.getElementById('y');
+    y.addEventListener('click', function() {
+        y.innerHTML = '100k-150k'
+    });
+}
+
+testing();
+
+
+
+
+
